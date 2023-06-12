c0354c82af7aba6b2617fda14fe2cab83782d8b0
Henry Quillin <henryquillin@gmail.com>
2023-06-12 11:24:46 -0500

    add logs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const pluginStealth = require("puppeteer-extra-plugin-stealth");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
   puppeteer.use(pluginStealth());
   const browser = await puppeteer.launch({
     headless: "new",
@@ -15,10 +15,13 @@ app.get('/', async (req, res) => {
   // c
 
   const page = await browser.newPage();
+  console.log('at new page')
   await page.goto('https://example.com');
+  console.log('at example.com')
 
   const title = await page.title();
   await browser.close();
+  console.log(`Title of the page was: ${title}`); 
 
   res.send(`Title of the page was: ${title}`);
 });
