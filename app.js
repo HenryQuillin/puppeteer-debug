d22f4dbe8f8e55e82371f758cb8818a950aecc93
Henry Quillin <henryquillin@gmail.com>
2023-06-12 14:07:36 -0500

    use puppeteer core

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const pluginStealth = require("puppeteer-extra-plugin-stealth");
 
 
